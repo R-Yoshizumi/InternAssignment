c9f89ff8903fccbc0810a313ff09a1d4e6f8e00d
R-Yoshizumi <r.yoshizumi@miraiplay.jp>
2023-07-11 13:11:59 +0900

    サインの保存処理

---
+++ Assets/Scripts/SaveSign.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+
+
+
+public class SaveSign : MonoBehaviour
+{    
+    [SerializeField] Image image;
+    private const string IMAGE_SAVE_FOLDER = "Image";
+    public void OnClick(){
+        TextureToPng(SavePath(IMAGE_SAVE_FOLDER));
+    }
+
+    private string SavePath(string folderName){
+        string directoryPath = Application.persistentDataPath + "/" + folderName + "/";
+
+        if (!Directory.Exists(directoryPath))
+        {
+            //まだ存在してなかったら作成
+            Directory.CreateDirectory(directoryPath);
+            return directoryPath + "paint.png";
+        }
+        Debug.Log(directoryPath+"に保存完了");
+
+        return directoryPath + "sign.png";
+    } 
+
+    private void TextureToPng(string path){
+        byte[] bytes=image.sprite.texture.EncodeToPNG();
+        File.WriteAllBytes(path, bytes);
+    }
+
+}
