095460d55adbaada1f7c739bb128731c6ff993d3
R-Yoshizumi <r.yoshizumi@miraiplay.jp>
2023-07-11 13:10:51 +0900

    ペイント処理の追加

--- Assets/Scripts/Paint.cs
+++ Assets/Scripts/Paint.cs
@@ -3,55 +3,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Drawing;
+
+using Color = UnityEngine.Color;
 
 public class Paint : MonoBehaviour
 {
-    [SerializeField] private RawImage image;
-    [SerializeField, Range(1,20)] private int brushSize=10;
+    [SerializeField] private Image image;
+    [SerializeField, Range(1,20)] private int brushSize=5;
 
-    int brush_min;
-    int brush_max;
-    private int test = 1;
+    int brushMin;
+    int brushMax;
 
 
-    private Texture2D texture = null;
+
+
+    private Texture2D signTexture = null;
     void Start()
     {
         var rect = image.gameObject.GetComponent<RectTransform>().rect;
-        //texture=new Texture2D((int)image.rectTransform.rect.width, (int)image.rectTransform.rect.height, TextureFormat.RGBA32, false);
-        texture=new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGBA32, false);
-        image.texture = texture;
+        signTexture=new Texture2D((int)rect.width, (int)rect.height, TextureFormat.RGBA32, false);
+        image.sprite = Sprite.Create(signTexture, new Rect(0, 0, signTexture.width, signTexture.height), Vector2.zero);
+        //image.sprite = signTexture;
 
     }
     
+    Vector2 beforePos;
+    Vector2 afterPos;
+
+    
+    
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+
+        //ブラシの大きさ変更
+        if(brushSize/2==0){
+            brushMin=1-(brushSize/2);
+            brushMax=brushSize/2;
+        }else{
+            brushMin=-brushSize/2;
+            brushMax=brushSize/2;
+        }
+
+        if(Input.GetMouseButtonDown(0))
         {
-            //Vector2 mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            var pos = Input.mousePosition;
-            
-            int brush_min;
-            int brush_max;    
-            //ブラシの大きさ変更
-            if(brushSize/2==0){
-                brush_min=1-brushSize/2;
-                brush_max=brushSize/2;
-            }else{
-                brush_min=-brushSize/2;
-                brush_max=brushSize/2;
-            }
-
-            for(int i=brush_min; i<=brush_max;i++){
-                for(int j=brush_min; j<=brush_max;j++){
-                    texture.SetPixel((int)pos.x+i, (int)pos.y-480+j, Color.red);
-                    //texture.SetPixel((int)mouse.x+i, (int)mouse.y+j, Color.red);
+            beforePos = Input.mousePosition;
+        }
+        else if(Input.GetMouseButton(0)){
+            afterPos = Input.mousePosition;
+            //座標がずれていたので一時的に調整
+            afterPos.x-=100;
+            afterPos.y+=300;
+            for(int i=brushMin; i<=brushMax;i++){
+                for(int j=brushMin; j<=brushMax;j++){
+                    signTexture.SetPixel((int)afterPos.x+i, (int)afterPos.y+j, Color.red);
                 }
             }
-
-            texture.Apply();
         }
+            signTexture.Apply();
     }
 
+    //線で描画して隙間をなくすための関数（途中）
+    public void DrawLine(Vector3 afterPoint,Vector3 beforePoint){
+       
+        Debug.Log("beforePosition"+beforePos);
+        Debug.Log("afterPosition"+afterPos);
+                
+    }
 }
+
+
